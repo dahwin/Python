ed3a5667faf95ceae4bb35b05ccd4a9db97a2020
dahwin <116961692+dahwin@users.noreply.github.com>
2023-10-22 14:31:23 +0600

    Update others.py

--- my python documentation/object and class/OOP Principles/others.py
+++ my python documentation/object and class/OOP Principles/others.py
@@ -1,25 +1,32 @@
-# class Dahwin():
-#     dubu:str='dubu'
-#     dahwin:str='dahwin'
-#     dahyun:str='dahyun'
-#     darwin:str='darwin'
+class Dahwin():
+    dubu:str='dubu'
+    dahwin:str='dahwin'
+    dahyun:str='dahyun'
+    darwin:str='darwin'
 
-import os
+    def call(self):
+        d = self.dubu
+        print(d)
+        return d
+dd = Dahwin()
+print(dd.call())
 
-# Specify the folder path you want to check
-folder_path = r"C:\Users\ALL USER\Desktop\Python"
+# import os
 
-# Specify the size threshold in bytes (100 MB)
-size_threshold = 100 * 1024 * 1024  # 100 MB in bytes
+# # Specify the folder path you want to check
+# folder_path = r"C:\Users\ALL USER\Desktop\Python"
 
-# Iterate over the files in the folder
-for root, dirs, files in os.walk(folder_path):
-    for file in files:
-        file_path = os.path.join(root, file)
-        file_size = os.path.getsize(file_path)
+# # Specify the size threshold in bytes (100 MB)
+# size_threshold = 100 * 1024 * 1024  # 100 MB in bytes
 
-        if file_size > size_threshold:
-            print(f"File: {file_path} is larger than 100 MB ({file_size / (1024*1024):.2f} MB)")
+# # Iterate over the files in the folder
+# for root, dirs, files in os.walk(folder_path):
+#     for file in files:
+#         file_path = os.path.join(root, file)
+#         file_size = os.path.getsize(file_path)
+
+#         if file_size > size_threshold:
+#             print(f"File: {file_path} is larger than 100 MB ({file_size / (1024*1024):.2f} MB)")
 
 
     
